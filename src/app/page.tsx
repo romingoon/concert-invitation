4ff0d1e3b944c68468eeab12efa7fab3c9f179ee
romingoon <romingoon@gmail.com>
2025-10-07 09:49:21 +0900

    메타 태그 재수정2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import type { Metadata } from 'next';
 
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
@@ -7,6 +8,28 @@ import { HomePage } from './components/pages/HomePage';
 import { ConcertInfoPage } from './components/pages/ConcertInfoPage';
 import { ProgramPage } from './components/pages/ProgramPage';
 import { LocationPage } from './components/pages/LocationPage';
+
+export const meta: Metadata = {
+  title: '주님과 함께 걷는 길은',
+  description:
+    '제5회 새로핌 교회음악 페스티벌 연주회 초대장 - 주님과 함께 걷는 길은 ',
+  openGraph: {
+    title: '주님과 함께 걷는 길은',
+    description: '제5회 새로핌 교회음악 페스티벌 연주회 초대장',
+    url: 'https://concert-invitation.vercel.app/',
+    siteName: '주님과 함께 걷는 길은',
+    images: [
+      {
+        url: '/images/thumnail.jpg',
+        width: 1200,
+        height: 630,
+        alt: '미리보기 이미지 설명',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+};
 
 export default function Page() {
   const [activeTab, setActiveTab] = useState('home');
