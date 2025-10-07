2eab3656f7ca4360ddf79f7909a4ee2b7035aba7
romingoon <romingoon@gmail.com>
2025-10-07 09:52:34 +0900

    메타데이터 재수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,28 @@
 'use client';
+import type { Metadata } from 'next';
 import './index.css';
+
+export const metadata: Metadata = {
+  title: '주님과 함께 걷는 길은',
+  description:
+    '제5회 새로핌 교회음악 페스티벌 연주회 초대장 - 주님과 함께 걷는 길은 ',
+  openGraph: {
+    title: '주님과 함께 걷는 길은',
+    description: '제5회 새로핌 교회음악 페스티벌 연주회 초대장',
+    url: 'https://concert-invitation.vercel.app/',
+    siteName: '주님과 함께 걷는 길은',
+    images: [
+      {
+        url: '/images/thumnail.jpg',
+        width: 1200,
+        height: 630,
+        alt: '미리보기 이미지 설명',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,6 @@ import { ConcertInfoPage } from './components/pages/ConcertInfoPage';
 import { ProgramPage } from './components/pages/ProgramPage';
 import { LocationPage } from './components/pages/LocationPage';
 
-export const meta: Metadata = {
-  title: '주님과 함께 걷는 길은',
-  description:
-    '제5회 새로핌 교회음악 페스티벌 연주회 초대장 - 주님과 함께 걷는 길은 ',
-  openGraph: {
-    title: '주님과 함께 걷는 길은',
-    description: '제5회 새로핌 교회음악 페스티벌 연주회 초대장',
-    url: 'https://concert-invitation.vercel.app/',
-    siteName: '주님과 함께 걷는 길은',
-    images: [
-      {
-        url: '/images/thumnail.jpg',
-        width: 1200,
-        height: 630,
-        alt: '미리보기 이미지 설명',
-      },
-    ],
-    locale: 'ko_KR',
-    type: 'website',
-  },
-};
-
 export default function Page() {
   const [activeTab, setActiveTab] = useState('home');
 
