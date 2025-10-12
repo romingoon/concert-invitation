225bb77f217e15fc3de5dd6c83a528f1a4c3d035
romingoon <romingoon@gmail.com>
2025-10-12 18:19:51 +0900

    카톡 공유 썸네일 이미지 경로 수정

--- src/app/components/ShareSection.tsx
+++ src/app/components/ShareSection.tsx
@@ -38,7 +38,7 @@ export function ShareSection({ title, url }: ShareSectionProps) {
       content: {
         title: '주님과 함께 걷는 길은 - 제5회 새로핌 교회음악 페스티벌 연주회',
         description: '2025년 10월 18일(토) 오후 3시\n새문안교회 1층 새문안홀',
-        imageUrl: `${baseUrl}/images/thumbnail.jpg`,
+        imageUrl: `${baseUrl}/images/thumnail.jpg`,
         link: {
           mobileWebUrl: currentUrl,
           webUrl: currentUrl,
